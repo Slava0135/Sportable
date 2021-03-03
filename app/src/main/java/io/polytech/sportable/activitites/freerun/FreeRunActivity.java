a9e31b0f8e80521d41f93b71b8dac68db7cf07a1
skwita <skwita.max@gmail.com>
2021-03-03 19:21:32 +0300

    Начало положено!

--- app/src/main/java/io/polytech/sportable/activitites/freerun/FreeRunActivity.java
+++ app/src/main/java/io/polytech/sportable/activitites/freerun/FreeRunActivity.java
@@ -7,10 +7,14 @@ import android.os.Bundle;
 import io.polytech.sportable.R;
 
 public class FreeRunActivity extends AppCompatActivity {
+    float distance = 0.0f;
+    int seconds = 0;
+    int calories = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_run);
     }
+
 }
