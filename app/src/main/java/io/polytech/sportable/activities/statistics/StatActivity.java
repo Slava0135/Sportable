b00405fdc8f38285be4ef7277667373555887251
DenZhid <denis.zhidkov01@gmail.com>
2021-03-05 22:47:43 +0300

    Some StatActivity.java fixes and some code CleanUp, can be pushed, if needed

--- app/src/main/java/io/polytech/sportable/activities/statistics/StatActivity.java
+++ app/src/main/java/io/polytech/sportable/activities/statistics/StatActivity.java
@@ -1,8 +1,12 @@
 package io.polytech.sportable.activities.statistics;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import io.polytech.sportable.R;
 
@@ -13,7 +17,24 @@ public class StatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stat);
         ViewPager2 viewPager = findViewById(R.id.stat_view);
+        TabLayout tabLayout = findViewById(R.id.tabs);
         viewPager.setAdapter(
                 new StatActivityPagerAdapter(getSupportFragmentManager(), getLifecycle()));
+        TabLayoutMediator mediator = new TabLayoutMediator(
+                tabLayout, viewPager, true, (tab, position) -> {
+                    switch (position) {
+                        case 0:
+                            tab.setText("Total");
+                            break;
+                        case 1:
+                            tab.setText("Certain Activity");
+                            break;
+                        case 2:
+                            tab.setText("Certain Day");
+                            break;
+                    }
+                    viewPager.setCurrentItem(tab.getPosition(), true);
+        });
+        mediator.attach();
     }
 }
