a58ffd72f65249974d4084b2e5ab2dae43dc530f
DenZhid <denis.zhidkov01@gmail.com>
2021-03-13 20:23:28 +0300

    Added maps + fixed bug in stats

--- app/src/main/java/io/polytech/sportable/activities/statistics/StatActivity.java
+++ app/src/main/java/io/polytech/sportable/activities/statistics/StatActivity.java
@@ -1,14 +1,20 @@
 package io.polytech.sportable.activities.statistics;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
+import java.util.List;
+
 import io.polytech.sportable.R;
+import io.polytech.sportable.persistence.PracticeRepository;
+import io.polytech.sportable.persistence.PracticeResult;
 
 public class StatActivity extends AppCompatActivity {
 
@@ -36,5 +42,11 @@ public class StatActivity extends AppCompatActivity {
                     viewPager.setCurrentItem(tab.getPosition(), true);
         });
         mediator.attach();
+        /*PracticeRepository local = new PracticeRepository(getApplication());
+        List<PracticeResult> localData = local.getAllPractices().getValue();
+        TextView totalAllCalories = findViewById(R.id.total_all_calories);
+        TextView totalAveragedCalories = findViewById(R.id.total_averaged_calories_in_one_workout);
+        TextView totalAllDistance = findViewById(R.id.total_all_distance);
+        TextView totalAveragedDistance = findViewById(R.id.total_averaged_distance_in_one_workout);*/
     }
 }
