6f03a75bdb671ecf27c11b9737f72a4b235b76aa
slava0135 <super.novalskiy_0135@inbox.ru>
2021-03-14 13:17:15 +0300

    rework

--- app/src/main/java/io/polytech/sportable/services/PracticeService.java
+++ app/src/main/java/io/polytech/sportable/services/PracticeService.java
@@ -1,6 +1,7 @@
 package io.polytech.sportable.services;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -21,15 +22,23 @@ import com.google.android.gms.location.LocationServices;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import io.polytech.sportable.models.practice.PracticeType;
+
 import static android.content.ContentValues.TAG;
 
 public class PracticeService extends Service {
 
-    private long mStartTimeMillis;
-    private volatile float distance;
+    private int time;
+    private float distance;
+    private PracticeType practiceType;
 
-    private Timer timer;
-    private UpdateDistanceTask task;
+    private Handler handler;
+    private Runnable locationUpdate;
+    private Runnable infoUpdate;
+    private boolean isRunning;
+
+    private int locatePeriod = 10000;
+    private int infoPeriod = 1000;
 
     private FusedLocationProviderClient mFusedLocationClient;
     private Location mLocation;
@@ -45,57 +54,82 @@ public class PracticeService extends Service {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        run();
         return mBinder;
     }
 
     @Override
     public void onCreate() {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        timer = new Timer();
-        task = new UpdateDistanceTask();
-    }
-
-    public void run() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        mStartTimeMillis = System.currentTimeMillis();
-        distance = 0;
-        mLocation = mFusedLocationClient.getLastLocation().getResult();
-        timer.schedule(task, 0, 10000);
+        handler = new Handler();
     }
 
     @Override
-    public void onDestroy() {
-        timer = null;
-        task = null;
-        mLocation = null;
-        mFusedLocationClient = null;
-        super.onDestroy();
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
     }
 
-    private class UpdateDistanceTask extends TimerTask {
-        public void run() {
-            if (ActivityCompat.checkSelfPermission(PracticeService.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(PracticeService.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            Location newLocation = mFusedLocationClient.getLastLocation().getResult();
-            distance += mLocation.distanceTo(newLocation);
-            mLocation = newLocation;
+    public void run(PracticeType practiceType) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
         }
+
+        time = 0;
+        distance = 0;
+        this.practiceType = practiceType;
+        isRunning = true;
+
+        handler = new Handler();
+
+        mLocation = mFusedLocationClient.getLastLocation().getResult();
+        locationUpdate = () -> {
+            if (isRunning) {
+                Location newLocation = mFusedLocationClient.getLastLocation().getResult();
+                distance += mLocation.distanceTo(newLocation);
+                mLocation = newLocation;
+                handler.postDelayed(locationUpdate, locatePeriod);
+            }
+        };
+        handler.post(locationUpdate);
+
+        infoUpdate = () -> {
+            if (isRunning) {
+                time += infoPeriod;
+                handler.postDelayed(infoUpdate, infoPeriod);
+            }
+        };
+    }
+
+    public void pause() {
+        isRunning = false;
+    }
+
+    @SuppressLint("MissingPermission")
+    public void resume() {
+        isRunning = true;
+        mLocation = mFusedLocationClient.getLastLocation().getResult();
     }
 
     public float getDistanceRunning() {
         return distance;
     }
-
-    public long getTimeRunning() {
-        return System.currentTimeMillis() - mStartTimeMillis;
+    public int getTimeRunning() {
+        return time;
+    }
+    public float getSpeedMetersPerSecond() {
+        return 1000 * distance / time;
+    }
+    public float getCalories() {
+        return 0;
     }
 
-    public void reset() {
-        distance = 0;
-        mStartTimeMillis = System.currentTimeMillis();
+    @Override
+    public void onDestroy() {
+        if (handler != null) {
+            handler.removeCallbacks(locationUpdate);
+        }
+        mLocation = null;
+        mFusedLocationClient = null;
+        super.onDestroy();
     }
 }
