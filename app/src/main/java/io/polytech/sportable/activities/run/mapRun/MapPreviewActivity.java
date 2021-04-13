3c36e67076733c6f6f0a7839b1fdeaa0fb709589
slava0135 <super.novalskiy_0135@inbox.ru>
2021-04-13 20:37:37 +0300

    preview

--- app/src/main/java/io/polytech/sportable/activities/run/mapRun/MapPreviewActivity.java
+++ app/src/main/java/io/polytech/sportable/activities/run/mapRun/MapPreviewActivity.java
@@ -4,13 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.mapview.MapView;
+
 import io.polytech.sportable.R;
 
 public class MapPreviewActivity extends AppCompatActivity {
 
+    MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        MapKitFactory.initialize(this);
         setContentView(R.layout.activity_map_preview);
+        super.onCreate(savedInstanceState);
+        mapView = findViewById(R.id.chooseMapview);
+    }
+
+    @Override
+    protected void onStop() {
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+        MapKitFactory.getInstance().onStart();
     }
 }
