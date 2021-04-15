b6704e6e2c93f610eb4cfcbdee703218a1b425db
Stoncs <andrej2002322@gmail.com>
2021-04-15 17:48:53 +0300

    Выведены формулы для разных углов наклолна для бега и ходьбы. Написаны псевдофункции для получения синуса, который потом используется в новых формулах для бега и ходьбы Настоящий программист никогда не делает все хорошо с первого раза, он понимает важность патчей.

--- app/src/main/java/io/polytech/sportable/models/practice/UserModel.java
+++ app/src/main/java/io/polytech/sportable/models/practice/UserModel.java
@@ -2,15 +2,58 @@ package io.polytech.sportable.models.practice;
 
 import android.content.SharedPreferences;
 
-public class UserModel {
+public class АгаUserModel {
 
     private final int height;
     private final float weight;
+
+//    private final float sin = getSin();
+//    private final boolean isUp;
 
     public UserModel(SharedPreferences settings) {
         this.height = settings.getInt("height", 0);
         this.weight = settings.getFloat("weight", 0);
     }
+
+//    public float getSin(/*класс местоположения*/ start, /*класс местоположения*/ end) {
+//        z1 = start.getAltitude();
+//        z2 = end.getAltitude();
+//        Hypotenuse = getDistance();
+//        this.isUp = (z2 >= z1);
+//        return abs(z1 - z2) / Hypotenuse;
+//    }
+//
+//
+//
+//    public float newGetCalories(PracticeType type, int time, float velocity) {
+//        if (velocity == 0) return 0f;
+//        switch (type) {
+//            case Walk: {
+//                if (isUp) {
+//                    return time / 60f * (0.035f * weight + 0.029f * (velocity * velocity / height) * weight)*(1f + 0.7f * sin);
+//                } else {
+//                    return time / 60f * (0.035f * weight + 0.029f * (velocity * velocity / height) * weight)*(1f - 1.3f * sin);
+//                }
+//
+//            }
+//            case Run: {
+//                if (isUp) {
+//                    return time / 60f * (0.035f * weight + 0.029f * (velocity * velocity / height) * weight)*(1f + 0.7f * sin);
+//                } else {
+//                    return time / 60f * (0.035f * weight + 0.029f * (velocity * velocity / height) * weight)*(1f - 1.3f * sin);
+//                }
+//            }
+//            case Bicycle: {
+//                return time / 60f * (5 * weight * 3.5f) / 200;
+//            }
+//            case Skies: {
+//                return time / 60f * (7 * weight * 3.5f) / 200;
+//            }
+//            default: {
+//                return 0f;
+//            }
+//        }
+//    }
 
     public float getCalories(PracticeType type, int time, float velocity) {
         if (velocity == 0) return 0f;
