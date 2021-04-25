112f00e43ca22abb84c7063439b022d226efdc7a
PolinaRubinova <jerrygonsales@mail.ru>
2021-04-26 00:00:32 +0300

    дезигн настроек профиля

--- app/src/main/java/io/polytech/sportable/activities/settings/FirstEntry.java
+++ app/src/main/java/io/polytech/sportable/activities/settings/FirstEntry.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
