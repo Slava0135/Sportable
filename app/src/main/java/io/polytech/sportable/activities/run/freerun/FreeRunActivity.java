6437529602d577ced7c4e02d66e6ea6f9c0a8016
skwita <skwita.max@gmail.com>
2021-05-12 15:01:03 +0300

    может влезло, может, нет

--- app/src/main/java/io/polytech/sportable/activities/run/freerun/FreeRunActivity.java
+++ app/src/main/java/io/polytech/sportable/activities/run/freerun/FreeRunActivity.java
@@ -103,9 +103,9 @@ public class FreeRunActivity extends AppCompatActivity {
                     int minutes = seconds / 60;
                     String time = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds % 60);
                     valueTime.setText(time);
-                    valueDistance.setText(String.format(Locale.getDefault(), "%.0f", model.mService.getDistanceMeters()));
-                    valueSpeed.setText(String.format(Locale.getDefault(), "%.1f", model.mService.getSpeedMetersPerSecond()));
-                    valueCalories.setText(String.format(Locale.getDefault(), "%.0f",  + model.mService.getCalories()));
+                    valueDistance.setText(String.format(Locale.getDefault(), "%.0f м.", model.mService.getDistanceMeters()));
+                    valueSpeed.setText(String.format(Locale.getDefault(), "%.1f м/с", model.mService.getSpeedMetersPerSecond()));
+                    valueCalories.setText(String.format(Locale.getDefault(), "%.0f ккал",  + model.mService.getCalories()));
                 }
                 handler.postDelayed(this, 1000);
             }
